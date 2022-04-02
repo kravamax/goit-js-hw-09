ac93510ebd7f26db99a1d73657d5089445e2f07a
Maksym Kravchenko <maks.cross@gmail.com>
2022-04-02 22:11:23 +0300

    Upd task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const ref = {
   start: document.querySelector('[data-start]'),
   stop: document.querySelector('[data-stop]'),
-  bodyBackground: document.body.style.backgroundColor,
+  bodyStyle: document.body.style,
 };
 let timerId = null;
 
@@ -10,23 +10,27 @@ ref.stop.addEventListener('click', onStopClick);
 ref.stop.disabled = true;
 
 function onStartClick() {
-  ref.start.disabled = true;
-  ref.stop.disabled = false;
+  const DELAY = 1000;
 
-  timerId = setInterval(changeBackground, 1000);
+  toggleButtons();
+
+  timerId = setInterval(changeBackground, DELAY);
 }
 
 function onStopClick() {
-  ref.start.disabled = false;
-  ref.stop.disabled = true;
-
+  toggleButtons();
   clearInterval(timerId);
 }
 
 function changeBackground() {
-  ref.bodyBackground = getRandomHexColor();
+  ref.bodyStyle.backgroundColor = getRandomHexColor();
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function toggleButtons() {
+  ref.start.disabled = ref.start.disabled ? false : true;
+  ref.stop.disabled = ref.stop.disabled ? false : true;
 }
